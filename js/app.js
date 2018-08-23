92bf095f3389f4acd2bf8267674174953723e317
CW53 <36382760+CW53@users.noreply.github.com>
2018-08-23 12:40:16 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -107,3 +107,10 @@ document.addEventListener('keyup', function(e) {
   player.handleInput(allowedKeys[e.keyCode]);
 });
 
+const player = new Hero(100, 80);
+const bug1 = new Enemy(-101, 0, 200);
+const bug2 = new Enemy(-101, 83, 300);
+const bug3 = new Enemy((-101*2.5), 83, 300);
+const allEnemies = [];
+allEnemies.push(bug1, bug2, bug3);
+console.log(allEnemies);
