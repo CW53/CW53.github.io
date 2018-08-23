a421539ae5343c26a6df0487e11a7f3975b77ffa
CW53 <36382760+CW53@users.noreply.github.com>
2018-08-23 13:57:31 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,42 +1,39 @@
-
-
 // Enemies our player must avoid
 class Enemy {
-  constructor (x, y, speed) {
-    this.x = x;
-    this.y = y + 55;
-    this.speed = speed;
-    this.sprite = 'images/enemy-bug.png';
-    this.step = 101;
-    this.boundary = this.step * 5;
-    this.resetPos = -this.step;
-  }
-render()  {
+    constructor(x, y, speed) {
+        this.x = x;
+        this.y = y + 55;
+        this.speed = speed;
+        this.sprite = 'images/enemy-bug.png';
+        this.step = 101;
+        this.boundary = this.step * 5;
+        this.resetPos = -this.step;
+    }
+    render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-update (dt) {
-  if (this.x < this.boundary) {
-    this.x += this.speed * dt;
-  }
-  else {
-    this.x = this.resetPos;
-  }
-};
+    };
+    update(dt) {
+        if (this.x < this.boundary) {
+            this.x += this.speed * dt;
+        } else {
+            this.x = this.resetPos;
+        }
+    };
 }
 
 // update () {
-  //for (let enemy of allEnemies) {
-    //if (this.y === enemy.y && (enemy.x + enemy.step > this.x && enemy.x < this.x + this.step)) {
-    //  console.log('Collide');
+//for (let enemy of allEnemies) {
+//if (this.y === enemy.y && (enemy.x + enemy.step > this.x && enemy.x < this.x + this.step)) {
+//  console.log('Collide');
 //  }
 //}
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    // if enemy is not passed boundary, move forward; increment x by speed * dt. else reset pos to start
+// You should multiply any movement by the dt parameter
+// which will ensure the game runs at the same speed for
+// all computers.
+// if enemy is not passed boundary, move forward; increment x by speed * dt. else reset pos to start
 
 
 
@@ -44,61 +41,66 @@ update (dt) {
 
 
 class Hero {
-  constructor(x, y) {
-    this.sprite = 'images/char-horn-girl.png';
-    this.step = 101;
-    this.jump = 83;
-    this.startX = this.step * 2;
-    this.startY = (this.jump * 4) + 55;
-    this.x = this.startX;
-    this.y = this.startY;
-}
-  render() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    constructor(x, y) {
+        this.sprite = 'images/char-horn-girl.png';
+        this.step = 101;
+        this.jump = 83;
+        this.startX = this.step * 2;
+        this.startY = (this.jump * 4) + 55;
+        this.x = this.startX;
+        this.y = this.startY;
+        this.victory = false;
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
 }
 
 update() {
-  for (let enemy of allEnemies) {
-    if (this.y === enemy.y && (enemy.x + enemy.step/2 > this.x && enemy.x < this.x + this.step/2) ) {
-      alert('Collision!')
-      this.reset();
+    for (let enemy of allEnemies) {
+        if (this.y === enemy.y && (enemy.x + enemy.step / 2 > this.x && enemy.x < this.x + this.step / 2)) {
+            alert('Collision!')
+            this.reset();
+            if (this.y === 55) {
+                this.victory = true;
+            }
+        }
     }
-  }
-  reset() {
+}
+reset() {
     this.y = this.startY;
     this.x = this.startX;
 };
 }
 
 /**
-* @param {string} input - Direction to travel
-*/
+ * @param {string} input - Direction to travel
+ */
 
 handleInput(input) {
-    switch(input) {
-    case 'left':
-      if (this.x > 0) {
-        this.x -= this.step;
-      }
-      break;
-    case 'up':
-      if (this.y > this.jump) {
-      this.y -= this.jump;
+    switch (input) {
+        case 'left':
+            if (this.x > 0) {
+                this.x -= this.step;
+            }
+            break;
+        case 'up':
+            if (this.y > this.jump) {
+                this.y -= this.jump;
+            }
+            break;
+        case 'right':
+            if (this.x < this.step * 4) {
+                this.x += this.step;
+            }
+            break;
+        case 'down':
+            if (this.y < this.jump * 4) {
+                this.y += this.jump;
+            }
+            break;
     }
-      break;
-    case 'right':
-      if (this.x < this.step * 4) {
-      this.x += this.step;
-    }
-      break;
-    case 'down':
-      if (this.y < this.jump * 4) {
-      this.y += this.jump;
-    }
-      break;
-  }
-  this.Hero = player.handleInput ();
+    this.Hero = player.handleInput();
 }
 }
 
@@ -119,12 +121,12 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-  player.handleInput(allowedKeys[e.keyCode]);
+    player.handleInput(allowedKeys[e.keyCode]);
 });
 const player = new Hero(100, 80);
 const bug1 = new Enemy(-101, 0, 200);
 const bug2 = new Enemy(-101, 83, 300);
-const bug3 = new Enemy((-101*2.5), 83, 300);
+const bug3 = new Enemy((-101 * 2.5), 83, 300);
 const allEnemies = [];
 allEnemies.push(bug1, bug2, bug3);
 console.log(allEnemies);
